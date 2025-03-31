f5aaa354b231fa0aa7d7396558726dfa32f901d6
Medmana <ahiffonman@gmail.com>
2025-03-31 19:02:25 +0100

    Creation de Patien

--- mrc-api/models/Patient.js
+++ mrc-api/models/Patient.js
@@ -5,32 +5,20 @@ const patientSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   birthDate: { type: Date, required: true },
   gender: { type: String, enum: ['male', 'female', 'other'], required: true },
-  bloodGroup: { type: String, enum: ['A+','A-','B+','B-', 'AB+','AB-','O+','O-']},
+  bloodGroup: { type: String, enum: ['A+','A-','B+','B-', 'AB+','AB-','O+','O-','']},
+  height: { type: Number, description: "Taille en cm" },
+  weight: { type: Number, description: "Poids en kg" },
+  diseases: [{
+    name: { type: String, required: true },
+    stage: { type: String, enum: ['débutant', 'intermédiaire', 'avancé', 'chronique',''] },
+    diagnosisDate: { type: Date },}],
   address: {
     street: String,
     city: String,
-    postalCode: String,
     country: { type: String, default: 'Bénin' }
   },
   phone: { type: String, required: true, match: [/^\+229\d{10}$/, 'Numéro de téléphone béninois invalide'] },
   email: { type: String, lowercase: true },
-  medicalHistory: {
-    chronicDiseases: [String],
-    allergies: [String],
-    familyHistory: String
-  },
-  kidneyDisease: {
-    stage: { type: Number, min: 1, max: 5 },
-    diagnosisDate: Date,
-    comorbidities: [String]
-  },
-  currentTreatments: [{
-    name: String,
-    dosage: String,
-    frequency: String,
-    startDate: Date
-  }],
-  attendingDoctor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   fileNumber: { type: String, unique: true, required: true },
   isActive: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
