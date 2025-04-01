b86c961144f47021c518b1aba425423f9141e9ad
Medmana <ahiffonman@gmail.com>
2025-04-01 13:58:58 +0100

    k

--- mrc-api/routes/patientRoutes.js
+++ mrc-api/routes/patientRoutes.js
@@ -7,20 +7,12 @@ const role = require('../middlewares/role');
 // Middleware d'authentification pour toutes les routes patients
 router.use(auth);
 router.get('/:id', auth, (req, res, next) => {
-  // L'admin peut accéder à tous les dossiers
   if (req.user.role === 'admin') {
     return Patient.findById(req.params.id)
       .then(patient => res.json(patient))
       .catch(next);
   }
-  
-  // Les médecins ne voient que leurs patients
-  Patient.findOne({ _id: req.params.id, doctor: req.user.id })
-    .then(patient => {
-      if (!patient) return res.sendStatus(404);
-      res.json(patient);
-    })
-    .catch(next);
+  return patientController.getPatientById(req, res, next); // Passez au contrôleur normal
 });
 
 // Routes
